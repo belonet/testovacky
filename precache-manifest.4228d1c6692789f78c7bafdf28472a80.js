ab997f8012efb11e64ffa49c80f53e458845bc54
BELOPOTOCAN, Patrik <belopotocan@WX-OR7506440.intranet.orange.sk>
2019-03-29 16:18:49 +0100

    Updates

---
+++ precache-manifest.4228d1c6692789f78c7bafdf28472a80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9346476d0176873d6622",
+    "url": "/testovacky/static/css/main.7539f0c0.chunk.css"
+  },
+  {
+    "revision": "9346476d0176873d6622",
+    "url": "/testovacky/static/js/main.787e1606.chunk.js"
+  },
+  {
+    "revision": "d5bbcc8da8169b7dd01d",
+    "url": "/testovacky/static/js/runtime~main.d8132861.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/testovacky/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testovacky/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ae379b1103999f2190ea4b17d635c64b",
+    "url": "/testovacky/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/belonet/testovacky/precache-manifest.ea413d10ff073d1062c0ce96298ed11c.js"
+  "/testovacky/precache-manifest.4228d1c6692789f78c7bafdf28472a80.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/belonet/testovacky/index.html", {
+workbox.routing.registerNavigationRoute("/testovacky/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
